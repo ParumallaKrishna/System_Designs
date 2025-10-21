de1f5ad82b4ffdd0cb7f56844bdf742f3d863086
ParumallaKrishna <sivakrishnachowdary2015@gmail.com>
2025-10-21 22:58:34 +0530

    deleting old code

--- userService/src/main/java/com/uber/system/userService/dto/RideCreateRequest.java
+++
@@ -1,9 +0,0 @@
-// src/main/java/com/uber/system/userService/dto/RideCreateRequest.java
-package com.uber.system.userService.dto;
-
-import lombok.Data;
-
-@Data
-public class RideCreateRequest extends RideQuoteRequest {
-    private Long riderId;
-}
--- userService/src/main/java/com/uber/system/userService/repository/RideRepository.java
+++
@@ -1,8 +0,0 @@
-package com.uber.system.userService.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.uber.system.userService.entity.Ride;
-
-public interface RideRepository extends JpaRepository<Ride, Long> {
-}
