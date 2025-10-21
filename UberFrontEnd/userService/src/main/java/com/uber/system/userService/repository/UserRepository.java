29c60eed2bc3ddf1acfc86a7c545662b3525cd20
ParumallaKrishna <sivakrishnachowdary2015@gmail.com>
2025-10-22 00:01:30 +0530

    removed extra files

--- UberFrontEnd/userService/src/main/java/com/uber/system/userService/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uber.system.userService.repository;
-
-import com.uber.system.userService.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-}
